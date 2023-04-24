0a0f7326e1f93fc8efe4fb75d39dd11683fec4cf
Tada Chaipanya <fendersdtua@hotmail.com>
2023-04-24 12:11:42 +0700

    use express.Router()

---
+++ routes/shop.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+// const rootDir = require("./util/path");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  // res.sendFile("/views/shop.html"); // This slash refers to our root folder on our OS
+
+  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+  // "__dirname", gives us the path of a file in which we use it.
+
+  // res.sendFile(path.join(rootDir, "views", "shop.html")); // You can use this instead
+});
+
+module.exports = router;
