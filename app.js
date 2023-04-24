9500148408ca12473cb94e27f845bb5c50792c4e
Tada Chaipanya <fendersdtua@hotmail.com>
2023-04-24 14:17:17 +0700

    ✨ Set up template engine, pug

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const path = require("path");
 
 const app = express();
 
+app.set("view engine", "pug");
+// set any values globally on ExpressJS.
+// is has many default properties you can use with set method.
+// "view engine", is a property allows us to tell ExpressJS for any dynamic templates we're trying to render.
+// "pug", second argument, tell that we wan to use pug, a template engine.
+
+app.set("views", "views"); // the default is 'views', but it your folder is not name 'view', change the second argument to your folder name.
+
 const adminRoute = require("./routes/admin");
 const shopRoute = require("./routes/shop");
 
