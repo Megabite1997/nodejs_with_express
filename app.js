e6050a99e83a6fa88dd2790a947e1263ea8b5252
Tada Chaipanya <fendersdtua@hotmail.com>
2023-04-24 12:10:11 +0700

    🎨 Set bodyParser to read req.body

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
+
+const adminRoute = require("./routes/admin");
+const shopRoute = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoute);
+app.use(shopRoute);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(3005);
