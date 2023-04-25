fd234a391fdc70dff6dabfff40a89ae61a3ae7dc
Tada Chaipanya <fendersdtua@hotmail.com>
2023-04-25 14:37:14 +0700

    🎨 Added logic in html pug

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/login", (req, res, next) => {
-  res.render("./pug/login", { shopName: "Mandarin Muffin" }); // Add dynamic content
+  res.render("./pug/login", {
+    shopName: "Mandarin Muffin",
+    products: [{ item: 1, price: 1.99 }],
+  }); // Add dynamic content
 });
 
 module.exports = router;
